4782f96c1cc5f6bdea5836a23a7f4f3ac58f4571
Melad998 <118486834+Melad998@users.noreply.github.com>
2024-11-27 17:12:02 +0300

    Update layout.jsx

--- ‏‏my-app/src/app/layout.jsx
+++ ‏‏my-app/src/app/layout.jsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir='rtl'>
       <link rel="icon" href="../../public/icon.svg" />
-      <meta property="og:image" content="https://zapay-self.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage1.c162e9a7.png&w=750&q=75" />
+      <meta property="og:image" content="https://zajil-express.com/_next/image?url=https%3A%2F%2Fbackend.zajil-express.com%2Fimg%2Fasset%2FYXNzZXRzL2RlbGl2ZXJlZC5qcGc%3D%3Fw%3D900%26h%3D613%26fit%3Dcrop-50-50%26s%3D9bfed752b143d264559d205209b0f89f&w=2048&q=75" />
       <body className={tajawal.className}>
       <Navbar/>
       {/* <NumTrack.Provider value={{NTrack,SetNtrack}}> */}
